71db3b98bcb647f0e0d4b189f3dcd67a2a9cc281
rockpell <aythffk@gmail.com>
2019-07-31 18:23:44 +0900

    선원 클릭시 명령창 나타나고 UI 아닌 부분 클릭시 명령창 사라지는 기능 적용

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject eventMenu;
+    [SerializeField] private GameObject selectMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void outrangeClick()
+    {
+        Debug.Log("out!");
+        selectMenu.SetActive(false);
+    }
+
+    public void showSelectMenu()
+    {
+        selectMenu.SetActive(true);
+    }
+}
