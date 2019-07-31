5835183a838051385ba8af74b70c7d032f198d49
rockpell <aythffk@gmail.com>
2019-07-31 19:01:42 +0900

    명령창 생성시 마우스 오른쪽 위에 나타나도록 설정

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,22 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private Canvas myCanvas;
     [SerializeField] private GameObject eventMenu;
     [SerializeField] private GameObject selectMenu;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void outrangeClick()
@@ -27,6 +29,13 @@ public class UIManager : MonoBehaviour
 
     public void showSelectMenu()
     {
+        Vector2 _size = selectMenu.GetComponent<RectTransform>().sizeDelta;
+
+        CanvasScaler scaler = myCanvas.GetComponentInParent<CanvasScaler>();
+        selectMenu.GetComponent<RectTransform>().anchoredPosition 
+            = new Vector2(Input.mousePosition.x * scaler.referenceResolution.x / Screen.width - scaler.referenceResolution.x / 2 + _size.x/2,
+            Input.mousePosition.y * scaler.referenceResolution.y / Screen.height - scaler.referenceResolution.y / 2 + _size.y/2);
+
         selectMenu.SetActive(true);
     }
 }
