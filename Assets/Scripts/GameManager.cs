c45f87c0d12e8ebbc20e59c26ee384918bb97e19
rockpell <aythffk@gmail.com>
2019-08-01 15:41:57 +0900

    GameManager에 돈, 식량 등의 정보 저장

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,18 +10,27 @@ public class GameManager : MonoBehaviour
     [SerializeField] private int endGenerateCount = 3;
     private int[] generateCount = new int[4]; // 각 방향 생성 개수, 위쪽 오른쪽 아래쪽 왼쪽 순서
 
+    private int initMoney = 100; // 초기 자금
+    private int nowMoney = 0; // 현재 자금
+    private int food = 0; // 식량
+
+    // 낚시대
+    // 요트
+    // 선원
+
     private void Awake()
     {
         if (instance == null)
             instance = this;
         else
             Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        nowMoney = initMoney;
     }
 
     // Update is called once per frame
@@ -46,5 +55,20 @@ public class GameManager : MonoBehaviour
                 generateCount[2] > endGenerateCount || generateCount[3] > endGenerateCount)
             return true;
         return false;
+    }
+
+    public int InitMoney {
+        get { return initMoney; }
+        set { initMoney = value; }
+    }
+
+    public int NowMoney {
+        get { return nowMoney; }
+        set { nowMoney = value; }
+    }
+
+    public int Food {
+        get { return food; }
+        set { food = value; }
     }
 }
