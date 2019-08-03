d21d81a49e072c6ab5f8f16c4bb3f281578a8623
kimdh <kimdhmath0@gmail.com>
2019-08-03 17:24:10 +0900

    mainmenue금액 분배

--- Assets/Scripts/MainMenue.cs
+++ Assets/Scripts/MainMenue.cs
@@ -7,10 +7,13 @@ public class MainMenue : MonoBehaviour
     public Button bar, wharf, dock, market;
     public Button engineer, mate, angler;
     public Button yacht_b, rafter;
-    public Button fishing_rod_a, fishing_rod_b, fishing_rod_c;
+    public Button fishing_rod_a, fishing_rod_b, fishing_rod_c, foodup, fooddown;
     public Button _return;
     public Button start;
+
     public GameManager gamemanager;
+
+    private FishingRodA FishingRodA;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +33,8 @@ public class MainMenue : MonoBehaviour
         fishing_rod_a.onClick.AddListener(Fishing_rod_aOnClick);//낚시대a
         fishing_rod_b.onClick.AddListener(Fishing_rod_bOnClick);//낚시대b
         fishing_rod_c.onClick.AddListener(Fishing_rod_cOnClick);//낚시대c
+        foodup.onClick.AddListener(FoodUpOnClick);//음식 구매
+        fooddown.onClick.AddListener(FoodDownOnClick);//음식 판매
 
         start.onClick.AddListener(StartOnClick);//출항
 
@@ -88,6 +93,9 @@ public class MainMenue : MonoBehaviour
         fishing_rod_b.interactable = true;
         fishing_rod_c.interactable = true;
 
+        fooddown.interactable = true;
+        foodup.interactable = true;
+
         bar.interactable = false;
         wharf.interactable = false;
         dock.interactable = false;
@@ -96,13 +104,26 @@ public class MainMenue : MonoBehaviour
     }
     void EngineerOnClick()
     {
+        if(gamemanager.InitMoney >= 80)
+        {
+            gamemanager.InitMoney = gamemanager.InitMoney - 80;
+        }
+        
     }
     void MateOnClick()
     {
+        if (gamemanager.InitMoney >= 80)
+        {
+            gamemanager.InitMoney = gamemanager.InitMoney - 80;
+        }
 
     }
     void AnglerOnClick()
     {
+        if (gamemanager.InitMoney >= 80)
+        {
+            gamemanager.InitMoney = gamemanager.InitMoney - 80;
+        }
 
     }
     void Yacht_bOnClick()
@@ -115,7 +136,15 @@ public class MainMenue : MonoBehaviour
     }
     void Fishing_rod_aOnClick()
     {
+        if(gamemanager.InitMoney > 5)
+        {
+            gamemanager.InitMoney = gamemanager.InitMoney- FishingRodA.Price;
+        }
 
+    }
+    void Fishing_rod_aStartOnClick()
+    {
+        gamemanager.setNowFishingRod(0);
     }
     void Fishing_rod_bOnClick()
     {
@@ -125,24 +154,63 @@ public class MainMenue : MonoBehaviour
     {
 
     }
+    void FoodUpOnClick()
+    {
+        if(gamemanager.InitMoney > 0)
+        {
+            gamemanager.InitMoney = gamemanager.InitMoney - 1;
+            gamemanager.Food += 1;
+        }
+    }
+    void FoodDownOnClick()
+    {
+        if(gamemanager.Food > 0)
+        {
+            gamemanager.InitMoney = gamemanager.InitMoney + 1;
+            gamemanager.Food -= 1;
+        }
+        
+    }
     void StartOnClick()
     {
 
+
+
+
+
+
+
+
+
+
+
+        fishing_rod_a.onClick.AddListener(Fishing_rod_aOnClick);
     }
     void _ReturnOnClick()
     {
+
+            
+
         bar.interactable = true;
         wharf.interactable = true;
         dock.interactable = true;
         market.interactable = true;
+
+
         _return.interactable = false;
         engineer.interactable = false;
         mate.interactable = false;
         angler.interactable = false;
+        
         yacht_b.interactable = false;
         rafter.interactable = false;
+
         fishing_rod_a.interactable = false;
         fishing_rod_b.interactable = false;
         fishing_rod_c.interactable = false;
+
+        fooddown.interactable = false;
+        foodup.interactable = false;
+        
     }
 }
