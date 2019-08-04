b141760b1d17eab40e97daf4abeec0c586175a4c
rockpell <aythffk@gmail.com>
2019-08-04 20:35:09 +0900

    목적지 도착 엔딩 기능 추가 항구가 미니맵에서 보이게 설정

---
+++ Assets/Scripts/EndPort.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPort : MonoBehaviour
+{
+    private bool isEndGame = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (!isEndGame && coll.transform.tag == "Player")
+        {
+            isEndGame = true;
+
+            GameManager.Instance.NowGameEnding = GameEnding.ARRIVE;
+        }
+    }
+}
