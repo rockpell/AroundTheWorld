868fc49eee5384c449d901e3ff884e2d26ef7d9f
rockpell <aythffk@gmail.com>
2019-08-04 20:48:24 +0900

    난파선 기능 추가(맵에 배치완료)

---
+++ Assets/Scripts/Wreck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wreck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //showSaveCrewEvent
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.transform.tag == "Player")
+        {
+            UIManager.Instance.showSaveCrewEvent();
+
+            Destroy(this.gameObject);
+        }
+    }
+}
