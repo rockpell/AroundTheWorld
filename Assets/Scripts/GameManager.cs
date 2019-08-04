10b1ecef2cebff43a3053471fc41b232d44e826a
rockpell <aythffk@gmail.com>
2019-08-05 02:03:17 +0900

    esc 키 입력시 게임 종료 기능 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -64,6 +64,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
         if (isGameStart)
         {
             sumDeltaTime += Time.deltaTime;
